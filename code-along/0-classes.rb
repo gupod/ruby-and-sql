b9899201405ebf4cdb8567968d2bdd80854b85af
gupod <gustavo.pires@kellogg.northwestern.edu>
2022-02-01 14:56:49 +0000

    ca1

--- code-along/0-classes.rb
+++ code-along/0-classes.rb
@@ -3,6 +3,22 @@
 # should be the top-level directory (i.e. /workspace/your-app/) and then run:
 # rails runner code-along/0-classes.rb
 
+food_tacos = "tacos"
+puts food_tacos.upcase
+
+food_sushi = "sushi"
+puts food_sushi.upcase
+
 # String Class
 
 # String Methods
+
+class Dog
+
+    def speak
+        puts "Woof"
+    end 
+end 
+
+jenkins = Dog.new
+jenkins.speak
